0c86f26aa0d06ba9e4edb1d2fa0f149f6e85b0ab
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-07-07 11:31:18 +0200

    Add capability and modality badge components

---
+++ components/capability-badge.tsx
@@ -0,0 +1,30 @@
+import { Capability } from "@/lib/schema";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
+import {
+  BrainIcon,
+  EyeIcon,
+  SquareFunctionIcon,
+  WrenchIcon,
+} from "lucide-react";
+
+export const CapabilityBadge = ({
+  className,
+  capability,
+}: React.ComponentProps<"span"> & {
+  capability: Capability;
+}) => {
+  return (
+    <Badge
+      key={capability}
+      variant="outline"
+      className={cn("p-1 text-muted-foreground", className)}
+    >
+      {capability === "tools" && <WrenchIcon />}
+      {capability === "vision" && <EyeIcon />}
+      {capability === "reasoning" && <BrainIcon />}
+      {capability === "embedding" && <SquareFunctionIcon />}
+    </Badge>
+  );
+};
---
+++ components/modality-badge.tsx
@@ -0,0 +1,32 @@
+import { Modality } from "@/lib/schema";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
+import {
+  FileTextIcon,
+  ImageIcon,
+  TypeIcon,
+  VideoIcon,
+  Volume2Icon,
+} from "lucide-react";
+
+export const ModalityBadge = ({
+  className,
+  modality,
+}: React.ComponentProps<"span"> & {
+  modality: Modality;
+}) => {
+  return (
+    <Badge
+      key={modality}
+      variant="outline"
+      className={cn("p-1 text-muted-foreground", className)}
+    >
+      {modality === "text" && <TypeIcon />}
+      {modality === "audio" && <Volume2Icon />}
+      {modality === "image" && <ImageIcon />}
+      {modality === "video" && <VideoIcon />}
+      {modality === "pdf" && <FileTextIcon />}
+    </Badge>
+  );
+};
