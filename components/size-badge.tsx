fdb27e3faf2eff2ad1d69f263cbe29a66ef91574
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-07-09 15:05:31 +0200

    feat: Add SizeBadge component

---
+++ components/size-badge.tsx
@@ -0,0 +1,27 @@
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
+export const SizeBadge = ({
+  className,
+  size,
+}: React.ComponentProps<"span"> & { size: string }) => {
+  const sizeFloat = parseFloat(size);
+  const unitMatch = size.match(/[a-zA-Z]+/);
+  const unit = unitMatch ? unitMatch[0].toUpperCase() : "";
+
+  return (
+    <Badge
+      variant="outline"
+      className={cn(
+        "p-1 text-[10px] text-muted-foreground",
+        (unit === "MB" || (unit === "GB" && sizeFloat < 4)) &&
+          "bg-emerald-400/10",
+        unit === "GB" && sizeFloat >= 4 && sizeFloat < 40 && "bg-amber-400/10",
+        unit === "GB" && sizeFloat >= 40 && "bg-rose-400/10",
+        className,
+      )}
+    >
+      {size}
+    </Badge>
+  );
+};
