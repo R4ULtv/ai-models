6978b0321abf053a0108375a1652bec9f0972cce
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-07-09 09:47:48 +0200

    feat: Add Ollama models page

---
+++ app/ollama/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import useSWRImmutable, { Fetcher } from "swr";
+import { useQueryState } from "nuqs";
+import { Suspense } from "react";
+
+import { DataTable } from "@/components/data-table";
+import { columns } from "@/components/columns";
+import { Model } from "@/lib/schema";
+import { TableSkeleton } from "@/components/table-skeleton";
+
+const fetcher: Fetcher<Model[], string> = (url) =>
+  fetch(url).then((res) => res.json());
+
+export default function Home() {
+  return (
+    <Suspense>
+      <Client />
+    </Suspense>
+  );
+}
+
+function Client() {
+  const [search] = useQueryState("search", { defaultValue: "" });
+
+  const { data, error, isLoading } = useSWRImmutable(
+    "/api/ollama/models.json",
+    fetcher,
+  );
+
+  if (isLoading) return <TableSkeleton rows={30} columns={13} />;
+
+  if (error) return <div>Error loading data</div>;
+
+  if (data && data.length > 0) {
+    return <DataTable columns={columns} data={data} search={search} />;
+  }
+}
